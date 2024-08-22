b570fe492ab83844b68c3bebb00f6a2e546a50aa
Olga Boyko <1586446@mail.ru>
2024-08-22 13:51:12 +0300

    Changed loading track schemes method

--- Formula 1/Data/Services/CloudStorageService.swift
+++ Formula 1/Data/Services/CloudStorageService.swift
@@ -15,29 +15,6 @@ final class CloudStorageService {
     func downloadFile(pathString: String) async throws -> Data {
         return try await storage.reference(withPath: pathString).data(maxSize: 1 * 1024 * 1024)
     }
-    
-    func fetchDriverImage(image: UIImage, driverID: String, completion: @escaping (Result<URL, Error>) -> Void) {
-        let storageRef = Storage.storage().reference().child("drivers/\(driverID).jpg")
-        if let imageData = image.jpegData(compressionQuality: 0.6) {
-            let metadata = StorageMetadata()
-            metadata.contentType = "image/jpg"
-            
-            storageRef.putData(imageData, metadata: metadata) { _, error in
-                if let error = error {
-                    completion(.failure(error))
-                    return
-                }
-                
-                storageRef.downloadURL { url, error in
-                    if let url = url {
-                        completion(.success(url))
-                    } else if let error = error {
-                        completion(.failure(error))
-                    }
-                }
-            }
-        }
-    }
 }
 
 enum CloudStorageServiceError: Error {
